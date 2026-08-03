7b7515bc5739ae447c718751576e98a9a670ca7b
MJ-Mncube <simamkelemncube@gmail.com>
2026-08-03 21:20:37 +0200

    Add project files.

---
+++ Municipality Service Request Management System X/Form1.cs
@@ -0,0 +1,10 @@
+namespace Municipality_Service_Request_Management_System_X
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
